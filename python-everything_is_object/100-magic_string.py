33b2266baf592c0b9b4ffc0825a7b3dd908af354
kethia19 <kkayigire@alustudent.com>
2025-03-07 14:08:26 -0800

    Updating files

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def magic_string(i=[0]):
-    i[0] += 1
-    return str("BestSchool, " * (i[0] - 1)) + "BestSchool"
+def magic_string(n=0):
+    return ", ".join(["BestSchool"] * (n + 1))
--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-""" LockedClass.
-"""
+"""A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
 
 
 class LockedClass:
-    __slots__= ['first_name']
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ["first_name"]
+
+    def __init__(self, first_name=""):
+        self.first_name = first_name
