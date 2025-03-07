3587ee71d2d6f8b5e2c997c844b034e37cbcebdd
kethia19 <kkayigire@alustudent.com>
2025-03-07 11:18:54 -0800

    Created 2 new files for python-classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Square Class.
+
+This is is module contains an empty class that defines a square.
+
+"""
+
+
+class Square:
+    """empty class that defines the blueprint of a square."""
+
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+THis folder has classes and objects with files coded in Python. These files have also documentations in them.
