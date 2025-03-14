9a1f99e4fc0fb4c1aaef7195e7ebcf7c1f149ef0
kethia19 <kkayigire@alustudent.com>
2025-03-14 12:52:43 -0700

    adding the 100th file

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Change the database character set and collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change the table character set and collation
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change the column character set and collation
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
