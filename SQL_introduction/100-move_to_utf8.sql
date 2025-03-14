683ef3609d45a570680fc7425ec79fd5a9feb19c
kethia19 <kkayigire@alustudent.com>
2025-03-14 14:11:09 -0700

    Editing again

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -3,8 +3,8 @@ USE hbtn_0c_0;  -- Select the database
 -- Change the database character set and collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Change the table character set and collation
+-- Change the table default character set and collation
 ALTER TABLE first_table DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Ensure the 'name' column uses the correct collation without explicitly setting CHARACTER SET
-ALTER TABLE first_table CHANGE name name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+-- Ensure 'name' column uses the correct collation without MySQL adding CHARACTER SET explicitly
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -3,5 +3,5 @@ SELECT city, ROUND(AVG(value), 4) AS avg_temp
 FROM temperatures
 WHERE month IN (7, 8)
 GROUP BY city
-ORDER BY max_temp DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
