a4abbcd78605ae0cd90a40eae27a57b5ad5d5124
kethia19 <kkayigire@alustudent.com>
2025-03-14 11:28:41 -0700

    Removing spaces

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -13,19 +13,4 @@ def write_file(filename="", text=""):
     """
 
     with open(filename, 'w+') as f:
-        return f.write(text)#!/usr/bin/python3
-    """Module 1-write_file.
-    Writes in a text file.
-    """
-
-
-    def write_file(filename="", text=""):
-            """Writes text in filename.
-                Args:
-                        - filename: name of the file
-                                - text: string to write in the file
-                                    Returns: number of characters written
-                                        """
-
-                                            with open(filename, 'w+') as f:
-                                                        return f.write(text)
+        return f.write(text)
