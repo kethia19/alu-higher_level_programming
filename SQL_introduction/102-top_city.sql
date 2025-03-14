9b54d7228bf737f24a4db8b2edea22e8832691e9
kethia19 <kkayigire@alustudent.com>
2025-03-14 13:55:40 -0700

    Changing max to average

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Displays the top 3 cities based on temperatures
-SELECT city, MAX(value) AS max_temp
+SELECT city, ROUND(AVG(value), 4) AS max_temp
 FROM temperatures
-WHERE month IN (7, 8)  -- Use 'month' instead of 'date'
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY max_temp DESC
 LIMIT 3;
