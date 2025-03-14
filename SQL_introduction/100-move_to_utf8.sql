938324201da27830426d651afb0ca46719426a05
kethia19 <kkayigire@alustudent.com>
2025-03-14 12:58:12 -0700

    adding the database used

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
+USE hbtn_0c_0;  -- Sets the default database used here
 -- Change the database character set and collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
