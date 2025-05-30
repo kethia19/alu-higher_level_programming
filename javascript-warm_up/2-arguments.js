5e8da9b30b32c81dac4a3dc23f7077559e55ab62
kethia19 <k.kayigire@alustudent.com>
2025-05-30 15:24:43 -0700

    Adding the rest basic tasks remaining

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+if (argv.length === 1) {
+	console.log('Argument found');
+} else if (argv.length > 1) {
+	console.log('Arguments found');
+} else {
+	console.log('No argument');
+}
